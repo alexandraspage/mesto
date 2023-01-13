6f26906776b961786d5129ee64ed4b45412f7a3e
Sasha Dudina <aleksad301@gmail.com>
2023-01-13 16:06:46 +0300

    First commit

---
+++ scripts/index.js
@@ -0,0 +1,54 @@
+const formElement = document.querySelector('.form');
+const formCloseButtonElement = formElement.querySelector('.form__close-button');
+const formOpenButtonElement = document.querySelector('.profile__edit-button');
+
+let nameInput = document.querySelector('.input__name');
+let jobInput = document.querySelector('.input__job');
+
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
+
+const openForm = function() {
+
+  formElement.classList.add('form_opened'); 
+
+  profileName.value;
+  profileJob.value;
+  
+  jobInput.value = profileJob.textContent;
+  nameInput.value = profileName.textContent;
+}
+
+const closeForm = function() {
+
+    formElement.classList.remove('form_opened'); 
+  }
+
+const closeFormFromButton = function(){
+
+  profileName.value;
+  profileJob.value;
+  
+  jobInput.value = profileJob.textContent;
+  nameInput.value = profileName.textContent;
+
+  formElement.classList.remove('form_opened'); 
+}
+
+formOpenButtonElement.addEventListener('click', openForm);
+formCloseButtonElement.addEventListener('click', closeFormFromButton);
+
+
+function changeInfo (evt){
+
+  evt.preventDefault();
+
+ nameInput.value;
+ jobInput.value;
+
+profileName.textContent = nameInput.value;
+profileJob.textContent = jobInput.value;
+closeForm();
+}
+
+formElement.addEventListener('submit', changeInfo); 
